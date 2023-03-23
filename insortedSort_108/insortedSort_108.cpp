599e6f4fd67f613b995a80428301cd3a5533a504
joisilham9 <127075874+joisilham9@users.noreply.github.com>
2023-03-23 21:22:42 +0700

    menyimpan nilai

--- insortedSort_108/insortedSort_108.cpp
+++ insortedSort_108/insortedSort_108.cpp
@@ -32,5 +32,20 @@ void insertionSort() {
 	int j;
 	for (int i = 1; i < n; i++) {
 		temp = arr[i];
-}
+
+		j = i - 1;
+
+		while (j >= 0 && arr[j] > temp) {
+			arr[j + 1] - arr[j];
+			j--;
+		}
+
+		arr[j + 1] - arr[j];
+
+		cout << endl;
+		cout << "\nPass " << i << ": ";
+		for (int k = 0; k < n; k++) {
+			cout << arr[i] << " ";
+		}
+     }
 }
