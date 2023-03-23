a0041e736f47a2ec9a21be81d69877b431ffda11
joisilham9 <127075874+joisilham9@users.noreply.github.com>
2023-03-23 21:06:37 +0700

    memasuakan variable sementara temp

--- insortedSort_108/insortedSort_108.cpp
+++ insortedSort_108/insortedSort_108.cpp
@@ -20,4 +20,17 @@ void input() {
 	cout << "==============================" << endl;
 	cout << "MASUAKKAN ELEMENT ARRAY" << endl;
 	cout << "==============================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
+}
+
+void insertionSort() {
+	int temp;
+	int j;
+	for (int i = 1; i < n; i++) {
+		temp = arr[i];
+}
 }
